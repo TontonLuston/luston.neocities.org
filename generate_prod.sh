32ab36150a3abdb19de2c9042465f562cc797077
Luston <bluestoneingener@gmail.com>
2024-07-28 16:43:42 +0200

    fix(workflow): update build and deploy workflow

--- generate_prod.sh
+++ generate_prod.sh
@@ -1,3 +1,1 @@
-./generate_prod_settings.sh $1
-
 vendor/bin/sculpin generate --env=prod
--- generate_prod_settings.sh
+++
@@ -1,7 +0,0 @@
-version_wildcard="#{version}"
-
-version=$1
-
-file="./app/config/sculpin_site_prod.yml"
-
-sed -i "s/$version_wildcard/$version/g" $file
