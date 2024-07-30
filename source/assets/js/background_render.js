a5a6ae89cc4a77af5e062510a8a86d328807bdd4
Luston <bluestoneingener@gmail.com>
2024-07-30 17:29:18 +0200

    fix(background_render): handle window size change

--- source/assets/js/background_render.js
+++ source/assets/js/background_render.js
@@ -1,39 +1,64 @@
 import * as THREE from 'three';
 
+function getSize() {
+    if (window.innerHeight < window.innerWidth) return window.innerHeight;
+    return window.innerWidth;
+}
+
+var viewSize = getSize();
+
+var rotation = 0;
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-jQuery('#background').html(renderer.domElement);
-
-const geometry = new THREE.CylinderGeometry( 1, 1, 5, 8 );
-const material = new THREE.MeshBasicMaterial( { wireframe: true } );
-const object = new THREE.Mesh( geometry, material );
-scene.add( object );
+const camera = new THREE.PerspectiveCamera( 55, 1, 0.1, 1000 );
 
 camera.position.z = 5;
 camera.rotation.z = 180;
 camera.rotation.x = 6.125
-object.rotation.x = 15;
 
+const renderer = new THREE.WebGLRenderer();
+
+function hasViewSizeChanged() {
+    return viewSize != getSize();
+}
+
+function updateViewSize() {
+    viewSize = getSize();
+
+    renderer.setSize(viewSize,  viewSize);
+}
+
+updateViewSize();
+
+jQuery('#background').html(renderer.domElement);
+
+const geometry = new THREE.CylinderGeometry( 1, 0.8, 5, 8 );
+const material = new THREE.MeshBasicMaterial( { wireframe: true } );
+const object = new THREE.Mesh( geometry, material );
+scene.add( object );
+
+object.rotation.x = 15;
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(object.rotation, {
-  scrollTrigger: {
-  trigger: "#trigger",
-  start: "top top",
-  end: "bottom top",
-  scrub: true,
-  toggleActions: "restart pause resume pause"
-},
-    y: Math.PI,
+ScrollTrigger.create({
+    trigger: "#trigger",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    onUpdate: (self) => rotation += self.progress,
 });
 
+
 function animate() {
+    if(hasViewSizeChanged()) {
+        updateViewSize();
+    }
+
     // object.rotation.x += 0.0050;
-    object.rotation.y += 0.0025;
+    rotation += 0.0025
+    object.rotation.y = rotation;
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
