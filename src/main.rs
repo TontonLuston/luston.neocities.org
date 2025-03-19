1e61ab469742fae74127f309e0228666340bdb7a
Luston <bluestoneingener@gmail.com>
2025-03-19 16:38:47 +0100

    feat(core): initialize rust, rocket and tera
    Initialize Rust project with Rocket framework, add basic routing, and set up Tera templates. Include .gitignore, Cargo.lock, and configuration files for VSCode and cspell.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket_dyn_templates::{Template, context};
+
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, hello])
+        .attach(Template::fairing())
+}
+
+#[get("/")]
+fn index() -> Template {
+    Template::render(
+        "index",
+        context! {
+            title: "luston.neocities.org",
+        },
+    )
+}
